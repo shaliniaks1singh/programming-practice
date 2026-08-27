cfcdd7bd3b512591a8c97bbb37fee93e09f8d9cc
Shalini Singh <shaliniaks1@gmail.com>
2026-08-27 12:32:57 +0530

    Add files via upload

---
+++ waveform.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+ using namespace std;
+  int main(){
+    int m,n;
+    cout<<"Enter the  row: "<<endl;
+    cin>>m;
+    cout<<"Enter the column: "<<endl;
+    cin>>n;
+    int arr[100][100];
+    cout<<"enter the values: "<<endl;
+    for(int i = 0 ;i<m;i++){
+        for(int j = 0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int j = 0 ;j<n;j++){
+        if(j%2==0){
+            for(int i = 0;i<n;i++){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        else{
+            for(int i = n-1 ; i>=0 ; i--){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+    }
+    return 0;
+}
